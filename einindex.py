0860cb360750d300ca50577b0a929d769fadaa84
Jon Malmaud <malmaud@gmail.com>
2018-12-16 14:36:22 -0500

    New grammar

--- einindex.py
+++ einindex.py
@@ -10,14 +10,30 @@ from functools import lru_cache
 
 
 @dataclass
-class VarList:
-    level: int
-    vars: List[str]
+class Var:
+    var: str
 
 
 @dataclass
-class IndexPattern:
-    source: List[VarList]
+class VarList:
+    vars: List[Var]
+
+
+@dataclass
+class IndexParam:
+    index: Var
+    index_vars: VarList
+
+
+@dataclass
+class Source:
+    main: VarList
+    indices: IndexParam
+
+
+@dataclass
+class IndexExpr:
+    source: Source
     target: VarList
 
 
@@ -29,16 +45,23 @@ def load_grammar():
 
 
 class Transformer(lark.Transformer):
-    def vargroup(self, vars):
-        return VarList(level=0, vars=[var.value for var in vars])
+    def varlist(self, vars):
+        return VarList(vars=[Var(var.value) for var in vars])
 
-    def bgroup(self, args):
-        varlist = args[1]
-        return VarList(level=varlist.level + 1, vars=varlist.vars)
+    def indexexpr(self, expr):
+        return Var(expr[1].value)
 
-    def start(self, args):
-        source, _, target = args
-        return IndexPattern(source=source.children, target=target.children)
+    def indexparam(self, params):
+        return IndexParam(index=params[0], index_vars=params[1])
+
+    def target(self, t):
+        return t[0]
+
+    def source(self, t):
+        return Source(main=t[0], indices=t[2])
+
+    def start(self, t):
+        return IndexExpr(source=t[0], target=t[2])
 
 
 def parse(pattern):
@@ -46,44 +69,3 @@ def parse(pattern):
     tree = grammar.parse(pattern)
     new_tree = Transformer().transform(tree)
     return new_tree
-
-
-def apply(pattern: IndexPattern, x, idx):
-    source = pattern.source
-    if len(source) == 1:
-        l = source[0]
-        if l.level == 1:
-            if len(l.vars) == 1:
-                return x[idx]
-    return apply_general(pattern, x, idx)
-
-
-def apply_general(pattern: IndexPattern, x, idx):
-    var_set = {}
-    for var_list in pattern.source:
-        if var_list.level == 1:
-            for var_idx, var_name in enumerate(var_list.vars):
-                var_set[var_name] = var_idx
-            break
-    out = x.new_zeros(size=idx.size())
-    size_set = [range(dim) for dim in idx.size()]
-    for i in itertools.product(*size_set):
-        idx_tuple = []
-        for var_list in pattern.source:
-            if var_list.level == 0:
-                for var in var_list.vars:
-                    pos = var_set[var]
-                    idx_tuple.append(i[pos])
-            elif var_list.level == 1:
-                inner_tuple = []
-                for var in var_list.vars:
-                    pos = var_set[var]
-                    inner_tuple.append(i[pos])
-                index_value = idx[tuple(inner_tuple)]
-                idx_tuple.append(index_value)
-        out[i] = x[tuple(idx_tuple)]
-    return out
-
-
-def index(pattern: str, x, idx):
-    return apply(parse(pattern), x, idx)
--- test_main.py
+++ test_main.py
@@ -5,25 +5,9 @@ import torch
 
 @pytest.fixture
 def pattern():
-    return "i, [i,j]->i,j"
+    return "i j, [j]ik ->ik"
 
 
 def test_parse(pattern):
     res = einindex.parse(pattern)
-    return res
-
-
-def test_apply(pattern):
-    parse = einindex.parse(pattern)
-    x = torch.tensor([[1, 2], [3, 4]]).float()
-    idx = torch.tensor([[1, 0], [0, 1]])
-    res = einindex.apply(parse, x, idx)
-    return res
-
-
-def test_slice():
-    pattern = "[i]->i"
-    x = torch.tensor([1, 2, 3, 4]).float()
-    idx = torch.tensor([0, 2])
-    res = einindex.index(pattern, x, idx)
     return res
